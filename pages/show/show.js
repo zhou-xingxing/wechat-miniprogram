e3101f5f1cee02d9cf4b639270a1f50ba3a63ce3
zhou-xingxing <jerry_xmz@163.com>
2021-03-19 22:00:21 +0800

    利用CSS知识优化了页面

--- pages/show/show.js
+++ pages/show/show.js
@@ -1,17 +1,22 @@
-// pages/weather/weather.js
+// pages/show/show.js
 Page({
-
+    // 事件处理函数
+    skipRandom() {
+      wx.navigateTo({
+        url: '../random/random'
+      })
+    },
+    // 事件处理函数
+    skipWeather() {
+      wx.navigateTo({
+        url: '../weather/weather'
+      })
+    },
   /**
    * 页面的初始数据
    */
   data: {
-    city:'',
-    update_time:'',
-    wea:'',
-    tem:'',
-    humidity:'',
-    win:'',
-    request:false
+
   },
 
   /**
@@ -32,26 +37,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    var that=this
-    wx.request({
-      url: 'https://tianqiapi.com/api?version=v6&appid=29866954&appsecret=QLD3si1i',
-      method:'GET',
-      success:function(res){
-        // console.log(res.data);
-        that.setData({
-          city:res.data.city,
-          update_time:res.data.update_time,
-          wea:res.data.wea,
-          tem:res.data.tem,
-          humidity:res.data.humidity,
-          win:res.data.win,
-          request:true,
-        })
-      },
-      fail:function(){
-        console.log('请求失败')
-      }
-    })
+
   },
 
   /**
